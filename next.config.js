f24c89eabf5f4fed64fa75dc189a73aba2614b3f
Marti Aguilera <mrtxaver@gmail.com>
2023-07-20 22:26:46 +0200

    Vercel Analytics añadido

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const withVercelAnalytics = require('@vercel/analytics');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = withVercelAnalytics(nextConfig);
